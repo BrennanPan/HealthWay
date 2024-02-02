3d25d01b135059043404ead96a0cd99b7b1d1c5a
Shaunak <shaunakduude@gmail.com>
2024-02-02 12:16:16 -0500

    Fixed CORS error - still needs to debug

--- backend/weatherAPI.js
+++ backend/weatherAPI.js
@@ -2,27 +2,9 @@ const axios = require('axios');
 
 const getWeather = (origin) => {
     const url = `http://api.weatherapi.com/v1/current.json?key=65002d70873d4054aff02044240302&q=${origin.lat},${origin.long}`
-    axios.get(url,{
-            header:{
-                'Content-Type': 'application/json'
-            }, data: {
-                "locations": [
-                    {
-                        "q": "53, -0.12",
-                        "custom_id" : "my-id-1"
-                    },
-                    {
-                        "q": "London",
-                        "custom_id": "any-internal-id"
-                    },
-                    {
-                        "q": "90201",
-                        "custom_id": "us-zipcode-id-765"
-                    }
-                ]
-            }})
+    axios.get(url)
     .then(response => {
-        console.log(response.data)
+        return response.data
     }).catch(err =>{
         console.log(err)
     })
