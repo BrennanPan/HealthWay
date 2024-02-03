7d77d9ccb702d441ce428118754896ddfc8c00f1
Brennan <brennan.pan3@gmail.com>
2024-02-03 17:06:35 -0500

    Edited object code

--- backend/pollenObj.js
+++ backend/pollenObj.js
@@ -1,8 +1,8 @@
 var pollenOBJ ={
-        grassRisk : "",
-        treeRisk : "",
-        weedRisk : "",
-        gassCount : 0,
-        treeCount : 0,
-        weedCount : 0,
+        "grassRisk" : "",
+        "treeRisk" : "",
+        "weedRisk" : "",
+        "gassCount" : 0,
+        "treeCount" : 0,
+        "weedCount" : 0,
 }
