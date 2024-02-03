172300b8d727a101d6ebcbef521626b6c59f5371
Brennan <brennan.pan3@gmail.com>
2024-02-03 13:45:48 -0500

    Fixed issues with pollenAPI

--- backend/pollenAPI.js
+++ backend/pollenAPI.js
@@ -1,9 +1,9 @@
 const axios = require("axios").default;
 
 const getData = (origin) => {
-    axios.get(`https://pollen.googleapis.com/v1/forecast:lookup?key=AIzaSyDPxFvD39w1dsueXMrpviPZGK1pS6DoyXY&location.longitude=35.32&location.latitude=32.32&days=1`)
+    return axios.get(`https://pollen.googleapis.com/v1/forecast:lookup?key=AIzaSyDPxFvD39w1dsueXMrpviPZGK1pS6DoyXY&location.longitude=${origin.long}&location.latitude=${origin.lat}&days=1`)
     .then(res => {
-        console.log(res.data.dailyInfo[0])
+        return res.data.dailyInfo[0]
     }).catch(err => {
         console.log(err)
     })
